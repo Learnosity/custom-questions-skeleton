41bf22fc4731d762ea226fcc3b02dc9a223f41ca
michael-fares-lrn <michael.fares@learnosity.com>
2022-11-11 15:40:49 -0600

    [FIX] make handle classes less verbose

--- demos/clock/src/question/components/utils/handleClasses.js
+++ demos/clock/src/question/components/utils/handleClasses.js
@@ -4,21 +4,11 @@
  * @param {array} elements _ array elements whose classnames we want to modify
  */
 export const showValidationUI = (correctAnswer, elements) => {
-    if (correctAnswer) {
-        elements.forEach((element) => {
-            if (element.classList.contains('lrn_incorrect')) {
-                element.classList.remove('lrn_incorrect');
-            }
-            element.classList.add('lrn_correct');
-        });
-    } else {
-        elements.forEach((element) => {
-            if (element.classList.contains('lrn_correct')) {
-                element.classList.remove('lrn_correct');
-            }
-            element.classList.add('lrn_incorrect');
-        });
-    }
+    elements.forEach((element) => {
+        correctAnswer
+            ? element.classList.add('lrn_correct')
+            : element.classList.add('lrn_incorrect');
+    });
 };
 
 export const clearValidationUI = (elements) => {
